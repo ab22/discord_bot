a4b63e3e43238e04ca6be312ef55257e9b39ab89
Abelardo E. Mendoza <abelardo22.9@gmail.com>
2019-09-04 14:21:50 -0600

    UI: added dark theme

--- ui/main.cpp
+++ ui/main.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDirIterator>
+#include <QFile>
+#include <QMessageBox>
+#include <QTextStream>
+#include <QtDebug>
 
 int main(int argc, char* argv[])
 {
-	QApplication a(argc, argv);
+	QApplication app(argc, argv);
 	MainWindow   w;
+	QFile        f(":qdarkstyle/style.qss");
+
+	if (!f.exists()) {
+		qDebug() << "Could not open theme file!";
+	} else {
+		f.open(QFile::ReadOnly | QFile::Text);
+		QTextStream ts(&f);
+		app.setStyleSheet(ts.readAll());
+    }
+
 	w.show();
 
-	return a.exec();
+	return app.exec();
 }
